8066eaa27358f4208482376096699877c2d9a4f4
Dhirendra <dhirendra.arjel@citytech.global>
2019-11-01 12:07:27 +0545

    Updates

---
+++ precache-manifest.ec5b52595f00d37272e1d972648db57f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748ec659e86ba971f7fe79326327bceb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "478eb028693b240c044f",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5385ac70fe7fbd5c9b44",
+    "url": "/portfolio/static/js/2.bd950f72.chunk.js"
+  },
+  {
+    "revision": "478eb028693b240c044f",
+    "url": "/portfolio/static/js/main.4d7c2076.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  }
+]);
